80f75808d5a066fcdeebeddc12dffb3efbdbadd0
attotem <vshinkarenko21@gmail.com>
2025-01-13 06:22:31 +0100

    cloudflare push tr1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 "use client"
 import Image from "next/image";
 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 "use client";
 import { useState } from "react";
 import AuthForm from "../../components/AuthForm";
